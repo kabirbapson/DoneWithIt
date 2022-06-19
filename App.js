9932628f2a5bfe0e6f2f13f085f8fa373099c62f
Bappi Kabir <kabirbapson@gmail.com>
2022-06-19 01:38:54 +0100

    added a some screen

--- App.js
+++ App.js
@@ -1,6 +1,52 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+// import React from "react";
+// import Screen from "./app/components/Screen";
+// import { Text, View } from "react-native";
+// import { createStackNavigator } from "@react-navigation/stack";
+// import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return <ListingEditScreen />;
+// const Tweets = () => (
+//   <Screen>
+//     <Text>Tweets</Text>
+//   </Screen>
+// );
+
+// const TweetDetails = () => (
+//   <Screen>
+//     <Text>Tweet Details</Text>
+//   </Screen>
+// );
+// const Stack = createStackNavigator();
+// const StackNavigator = () => (
+//   <Stack.Navigator initialRouteName={Tweets}>
+//     <Stack.Screen name="Tweets" component={Tweets} />
+//     <Stack.Screen name="TweetDetails" component={TweetDetails} />
+//   </Stack.Navigator>
+// );
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <StackNavigator />
+//     </NavigationContainer>
+//   );
+// }
+
+// In App.js in a new project
+
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
